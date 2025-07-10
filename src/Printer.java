ac5da09b466e5cadc16532460bd0bef5eb1c384a
its-spark-dev <its.spark.dev@gmail.com>
2025-07-10 03:57:56 -0400

    fix: Correct attempt count calculation in game result
    - Reset inning counter initial value to 1
    - Adjust win message to show correct attempt count

--- src/Printer.java
+++ src/Printer.java
@@ -1,7 +1,7 @@
 public class Printer {
 
     // 1. 메인 메소드에서 레프리로 부터 념겨받은 S/B/O 카운트를 회차와 함께 출력하기
-    int inning = 0;
+    int inning = 1;
 
     public void result(int s, int b, int o) {
         System.out.println("[Inning: " + inning + "] " + s + "S " + b + "B " + o + "O");
@@ -10,7 +10,7 @@ public class Printer {
 
 
     public void displayWin() {
-        System.out.println("You won in " + inning + " attempts!");
+        System.out.println("You won in " + (inning - 1) + " attempts!");
     }
 
     // 숫자 야구는 진다는 개념이 없고, 숫자를 맞추는 것 까지 얼마나 오래 걸렸냐는 것만 판단 함.
